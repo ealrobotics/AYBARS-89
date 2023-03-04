cf67904323386a79d6080513f5fe01153416092e
Talha Karasu <talha@karasu.xyz>
2023-03-05 00:40:44 +0300

    add compressor status to dashboard

--- src/main/java/frc/robot/subsystems/Infrastructure.java
+++ src/main/java/frc/robot/subsystems/Infrastructure.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANIDConstants;
 
@@ -10,5 +11,10 @@ public class Infrastructure extends SubsystemBase {
 
   public Infrastructure() {
     m_compressor.enableDigital();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("Compressor", m_compressor.isEnabled());
   }
 }
